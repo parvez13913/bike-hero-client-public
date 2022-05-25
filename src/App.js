23fc08d2b5400f485d6f066fc858008dbd243c60
parvez13913 <parvezz13913@gmail.com>
2022-05-25 02:15:02 -0700

    Not Found page Complite

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Pages/Home/Home/Home';
 import NavBar from './Pages/Shared/NavBar/NavBar';
 import Footer from './Pages/Shared/Footer/Footer';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <NavBar></NavBar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <div className='bg-dark p-5 fw-bold mt-5'>
+            <div className='bg-dark p-5 fw-bold'>
                 <h1 className='text-secondary text-center'>Bike Hero</h1>
                 <div className=' container w-100 text-center'>
                     <p className='text-secondary'>CBF offers you an amazing selection of high-performance brake pads, disks, and other accessories to improve the functionality of motorcycles at competitive prices. Parts are sourced from the most reputable manufacturers in the market, making sure we have the right brands for any model of motorcycle.We also cater to creating custom brake parts, specifically catering to those looking for high-quality aftermarket parts. Talk to our team today and see how our brake parts and accessories can match your needs.</p>
