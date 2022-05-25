efaa8e8c12367008a4dc06dab4d72c73769d625e
parvez13913 <parvezz13913@gmail.com>
2022-05-25 02:05:50 -0700

    Footer Complite

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
 import NavBar from './Pages/Shared/NavBar/NavBar';
+import Footer from './Pages/Shared/Footer/Footer';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='bg-dark p-5 fw-bold mt-5'>
+                <h1 className='text-secondary text-center'>Bike Hero</h1>
+                <div className=' container w-100 text-center'>
+                    <p className='text-secondary'>CBF offers you an amazing selection of high-performance brake pads, disks, and other accessories to improve the functionality of motorcycles at competitive prices. Parts are sourced from the most reputable manufacturers in the market, making sure we have the right brands for any model of motorcycle.We also cater to creating custom brake parts, specifically catering to those looking for high-quality aftermarket parts. Talk to our team today and see how our brake parts and accessories can match your needs.</p>
+                    <h5 className='text-secondary text-center fw-bold'>Contact Us</h5>
+                    <h5 className='text-secondary'>Email: parvezz13913@gmail.com</h5>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
